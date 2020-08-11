836fe11164a007531790b1c354336245641306d0
J Perump <jperump@gmail.com>
2020-08-10 19:16:01 -0700

    adding docker file and docker compose

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 
 const port = process.env.PORT || 3000;
-let uri = 'mongodb://localhost:27017/photoApp';
+let uri = 'mongodb://mongodb:27017/photoApp';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 
