8a5ffc1528f353b64d9b2905a84cb27956ce759b
Neeraja <neerajajithinp@gmail.com>
2020-08-27 18:40:55 -0700

    Modified User Database

--- models/user.js
+++ models/user.js
@@ -4,23 +4,46 @@ var bcrypt = require('bcrypt-nodejs')
 
 var userSchema = mongoose.Schema({
 
-  local            : {
-      email        : String,
-      password     : String,
-  },
-  created: {type: Date, default: Date.now}
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  created: {type: Date, default: Date.now},
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
+  
 });
 
-userSchema.methods.generateHash = function(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+userSchema.pre('save', function(next) {
+  var user = this;
+  var SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
 
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.local.password);
-};
+// userSchema.methods.generateHash = function(password) {
+//   return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+// };
 
-// userSchema.plugin(passportLocalMongoose);
+// userSchema.methods.validPassword = function(password) {
+//   return bcrypt.compareSync(password, this.local.password);
+// };
 
 // create the model for ADMINS and expose it to our app
 module.exports = mongoose.model('User', userSchema);
